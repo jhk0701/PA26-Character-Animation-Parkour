6f3f18fcdb32eeffffa5a531edc9b83c596bc034
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-07-07 17:10:55 +0900

    Update: .mini 좌표계 구성 수정

--- MiniEngine/Pakour/MiniEngine/Content/TestScene.cpp
+++ MiniEngine/Pakour/MiniEngine/Content/TestScene.cpp
@@ -29,7 +29,7 @@ void TestScene::Construct()
 		pCamComp->aspect = static_cast<float>(WINDOW_WIDTH) / static_cast<float>(WINDOW_HEIGHT);
 		pCamComp->RegisterMainCamera();
 
-		pCamActor->GetRoot()->localTransform.position = Vector3(0.0f, 1.0f, 5.0f);
+		pCamActor->GetRoot()->localTransform.position = Vector3(0.0f, 1.0f, -10.0f);
 		// m_camController.Initialize(Vector3(0.0f, 0.0f, 0.0f), 10.0f);
 	}
 
@@ -67,7 +67,7 @@ void TestScene::Construct()
 
 		std::shared_ptr<RigidBodyComponent> pRB = pCube->AddComponent<RigidBodyComponent>();
 		pRB->Init(*physics, RigidBodyComponent::EBodyType::Dynamic, half, 1.f);
-		// pRB->SetCollsionGroup(Physics::ECollisionGroup::IgnoreAll);
+		// pRB->SetCollsionGroup(Physics::ECollisionGroup::IgnoreAll); // 충돌그룹 확인용 테스트
 	}
 
 	// 중간 장애물
@@ -78,12 +78,12 @@ void TestScene::Construct()
 	// 사람 크기 장애물
 	std::shared_ptr<Actor> pObsTop = BuildObstacle(L"obstacle_top.mini");
 	std::shared_ptr<SceneComponent> pObsTopRoot = pObsTop->GetRoot();
-	pObsTopRoot->localTransform.position = Vector3(0.0f, 0.0f, -5.0f);
+	pObsTopRoot->localTransform.position = Vector3(0.0f, 0.0f, 5.0f);
 
 	// 사람보다 큰 장애물
 	std::shared_ptr<Actor> pObsHigh = BuildObstacle(L"obstacle_high.mini");
 	std::shared_ptr<SceneComponent> pObsHighRoot = pObsHigh->GetRoot();
-	pObsHighRoot->localTransform.position = Vector3(0.0f, 0.0f, -10.0f);
+	pObsHighRoot->localTransform.position = Vector3(0.0f, 0.0f, 10.0f);
 }
 
 std::shared_ptr<Actor> TestScene::BuildObstacle(const wchar_t* _path)
