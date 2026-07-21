f5513633373b5c95126aa8995d32c5c5c9bc0306
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-07-21 18:45:16 +0900

    Remove: 미사용 클래스 제거

--- MiniEngine/Pakour/MiniEngine/Content/TransitionState.cpp
+++ MiniEngine/Pakour/MiniEngine/Content/TransitionState.cpp
@@ -10,12 +10,3 @@ void TransitionState::Activate(MiniEngine::AnimNotifyParam& _param)
 	Character* pChar = dynamic_cast<Character*>(_param.m_pActor);
 	pChar->TransitionStateMachine(m_targetState);
 }
-
-void EnableHangingState::Activate(MiniEngine::AnimNotifyParam& _param)
-{
-	if (!_param.m_pActor)
-		return;
-
-	Character* pChar = dynamic_cast<Character*>(_param.m_pActor);
-	pChar->TransitionStateMachine((uint8_t)(m_bEnable ? Character::EState::Hanging : Character::EState::Landing));
-}
--- MiniEngine/Pakour/MiniEngine/Content/TransitionState.h
+++ MiniEngine/Pakour/MiniEngine/Content/TransitionState.h
@@ -10,13 +10,3 @@ public:
 private:
 	uint8_t m_targetState{ 0U };
 };
-
-class EnableHangingState : public MiniEngine::AnimNotify
-{
-public:
-	void Activate(MiniEngine::AnimNotifyParam& _param) override;
-	void SetEnable(bool _bEnable) { m_bEnable = _bEnable; }
-
-private:
-	bool m_bEnable{ true };
-};
