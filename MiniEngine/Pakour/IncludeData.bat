e3163145e25be34dc7af88db8f501a28921ce1cb
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-08-20 18:19:30 +0900

    Chore: 빌드 시, assets 폴더 포함

--- MiniEngine/Pakour/IncludeData.bat
+++ MiniEngine/Pakour/IncludeData.bat
@@ -1,5 +1,5 @@
-if not exist "$(OutDir)\Datas\" (
-    mkdir "$(OutDir)\Datas\"
-)
+if not exist "$(OutDir)\Datas\" ( mkdir "$(OutDir)\Datas\" )
+if not exist "$(OutDir)\Assets\" ( mkdir "$(OutDir)\Assets\" )
 
 XCOPY $(SolutionDir)\Datas\*.* $(OutDir)\Datas\*.* /E /H /Y
+XCOPY $(SolutionDir)\Assets\*.* $(OutDir)\Assets\*.* /E /H /Y
